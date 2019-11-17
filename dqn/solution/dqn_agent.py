db4c11b5810fa97ada7645d5cfb2a11bdf2709f6
VVKot <volodymyr.kot.ua@gmail.com>
2019-11-17 13:29:34 +0200

    feat: propage more changes for each episode
    Increase batch size and soft update coefficient such that each batch of updates propagates more changes through the network

--- dqn/solution/dqn_agent.py
+++ dqn/solution/dqn_agent.py
@@ -9,9 +9,9 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 BUFFER_SIZE = int(1e5)  # replay buffer size
-BATCH_SIZE = 64         # minibatch size
+BATCH_SIZE = 128         # minibatch size
 GAMMA = 0.99            # discount factor
-TAU = 1e-3              # for soft update of target parameters
+TAU = 1e-2              # for soft update of target parameters
 LR = 5e-4               # learning rate 
 UPDATE_EVERY = 4        # how often to update the network
 
